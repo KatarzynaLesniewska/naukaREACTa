bb116a17de4696119bcb0f8b1dbe02439b6546b0
Kasia Leśniewska <klesniewska29@gmail.com>
2020-06-20 13:59:10 +0200

    Debug: move <div> with children prop from Hero to List and place it under <Hero /> component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,6 @@ const Hero = (props) => (
   <header className={styles.component}>
     <h2 className={styles.title}>{props.titleText}</h2>
     <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" />
-    <div className={styles.description}>
-      {this.props.children}
-    </div>
   </header>
 );
 
