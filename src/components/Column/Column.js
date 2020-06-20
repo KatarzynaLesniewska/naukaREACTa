f9581cce8946287c8df0bb9493f02726a28faf4d
Kasia Leśniewska <klesniewska29@gmail.com>
2020-06-20 17:53:28 +0200

    Add 3 Columns with different titles ??????????????

---
+++ src/components/Column/Column.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Column.scss';
+
+class Column extends React.Component {
+    static propTypes = {
+        column: PropTypes.node,
+    }
+
+    render() {
+        return (
+            <section className={styles.component}>
+            <h3 className={styles.title}>Hello world!</h3>
+            </section>
+        );
+    }
+}
+
+export default Column;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
+import Column from '../Column/Column.js';
 
 class List extends React.Component {
   static propTypes = {
@@ -20,9 +21,15 @@ class List extends React.Component {
       <section className={styles.component}>
         <Hero 
         titleText={this.props.title} 
-        imgUrlHreo={this.props.imageURL} />
+        imgUrlHreo={this.props.imageURL} 
+        />
         <div className={styles.description}>
           {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          <Column title="Animals"></Column>
+          <Column title="Plants"></Column>
+          <Column title="Minerals"></Column>
         </div>
       </section>
     )
